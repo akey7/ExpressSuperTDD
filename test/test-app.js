3dbfe9437934428bbc5f5d3a1dd7d161759a0d27
Alicia Key <alicia.key@galvanize.com>
2018-11-30 11:21:00 -0700

    First test and knex

--- test/test-app.js
+++ test/test-app.js
@@ -4,3 +4,29 @@ const request = require('supertest')
 const expecte = require('chai').expect
 const app = require('../app')
 const knex = require('../knex')
+
+
+describe('GET /sloths', () => {
+  it('responds with JSON', done => {
+    request(app)
+      .get('/sloths')
+      .expect('Content-Type', /json/)
+      .expect(200, done)
+  })
+})
+
+xdescribe('GET /sloths/:id', () => {
+
+})
+
+xdescribe('POST /sloths', () => {
+
+})
+
+xdescribe('PUT /sloths/:id', () => {
+
+})
+
+xdescribe('DELETE /sloths/:id', () => {
+
+})
